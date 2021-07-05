ffec997bff40c7dba2b29005528c6a5893a11849
iynaur <iynaur87@sina.com>
2021-07-05 08:53:48 +0800

    pre process

--- cam.py
+++ cam.py
@@ -104,6 +104,16 @@ def EffModel(net_name):
         model_ft = model_ft.cuda()
     return model_ft
 
+def toStdInputCv(f):
+    orig = Image.open(f)
+    tr = transforms.Compose([
+            transforms.Resize(resize_size),
+            transforms.CenterCrop(input_size),
+        ]
+        )
+    inputs = tr(orig)
+    inputs.save('tmp.bmp')
+    return cv2.imread('tmp.bmp', 1)
 
 if __name__ == '__main__':
     """ python cam.py -image-path <path_to_image>
@@ -144,39 +154,39 @@ if __name__ == '__main__':
                                 target_layer=target_layer,
                                 use_cuda=args.use_cuda)
 
-        rgb_img = cv2.imread(args.image_path, 1)[:, :, ::-1]
+        rgb_img = toStdInputCv(args.image_path)[:, :, ::-1]
         rgb_img = np.float32(rgb_img) / 255
         input_tensor = preprocess_image(rgb_img, mean=[0.485, 0.456, 0.406],
                                                 std=[0.229, 0.224, 0.225])
 
         # If None, returns the map for the highest scoring category.
         # Otherwise, targets the requested category.
-        target_category = None
+        for target_category in [None,]:
 
-        # AblationCAM and ScoreCAM have batched implementations.
-        # You can override the internal batch size for faster computation.
-        cam.batch_size = 32
+            # AblationCAM and ScoreCAM have batched implementations.
+            # You can override the internal batch size for faster computation.
+            cam.batch_size = 32
 
-        grayscale_cam = cam(input_tensor=input_tensor,
-                            target_category=target_category,
-                            aug_smooth=args.aug_smooth,
-                            eigen_smooth=args.eigen_smooth)
+            grayscale_cam = cam(input_tensor=input_tensor,
+                                target_category=target_category,
+                                aug_smooth=args.aug_smooth,
+                                eigen_smooth=args.eigen_smooth)
 
-        # Here grayscale_cam has only one image in the batch
-        grayscale_cam = grayscale_cam[0, :]
+            # Here grayscale_cam has only one image in the batch
+            grayscale_cam = grayscale_cam[0, :]
 
-        cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+            cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
 
-        # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
-        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+            # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
+            cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
 
-        gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
-        gb = gb_model(input_tensor, target_category=target_category)
+            gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
+            gb = gb_model(input_tensor, target_category=target_category)
 
-        cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
-        cam_gb = deprocess_image(cam_mask * gb)
-        gb = deprocess_image(gb)
+            cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+            cam_gb = deprocess_image(cam_mask * gb)
+            gb = deprocess_image(gb)
 
-        cv2.imwrite(f'{f}_cam.jpg', cam_image)
-        cv2.imwrite(f'{f}_gb.jpg', gb)
-        cv2.imwrite(f'{f}_cam_gb.jpg', cam_gb)
+            cv2.imwrite(f'{f}_{target_category}_cam.jpg', cam_image)
+            # cv2.imwrite(f'{f}_{target_category}gb.jpg', gb)
+            # cv2.imwrite(f'{f}_{target_category}cam_gb.jpg', cam_gb)
